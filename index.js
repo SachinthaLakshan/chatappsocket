f4b18a76ae9364792b9480d0d368438644f09bf8
SachinthaLakshan <sachintha.kahagalla@gmail.com>
2024-02-24 17:17:44 +0530

    project chaged

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const server = http.createServer(app)
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: "*",
 		methods: [ "GET", "POST" ]
 	}
 })
@@ -25,4 +25,4 @@ io.on("connection", (socket) => {
 	})
 })
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(process.env.PORT || 9001, () => console.log("server is running on port 5000"))
